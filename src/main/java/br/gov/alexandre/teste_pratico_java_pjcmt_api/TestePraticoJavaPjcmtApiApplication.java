54d686e53e8de39564e1decd5cf08bb5ca1571f4
Alexandre Abreu <abreuxandi2010@gmail.com>
2024-06-19 17:14:49 -0300

    initial project

---
+++ src/main/java/br/gov/alexandre/teste_pratico_java_pjcmt_api/TestePraticoJavaPjcmtApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.alexandre.teste_pratico_java_pjcmt_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePraticoJavaPjcmtApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestePraticoJavaPjcmtApiApplication.class, args);
+    }
+
+}

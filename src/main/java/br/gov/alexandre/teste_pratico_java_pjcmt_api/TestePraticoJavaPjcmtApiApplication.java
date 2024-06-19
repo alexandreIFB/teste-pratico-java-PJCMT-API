a60702e0e3e1459526e4406502880080bed12507
Alexandre Abreu <abreuxandi2010@gmail.com>
2024-06-19 18:15:40 -0300

    add spring web

--- src/main/java/br/gov/alexandre/teste_pratico_java_pjcmt_api/TestePraticoJavaPjcmtApiApplication.java
+++ src/main/java/br/gov/alexandre/teste_pratico_java_pjcmt_api/TestePraticoJavaPjcmtApiApplication.java
@@ -2,6 +2,9 @@ package br.gov.alexandre.teste_pratico_java_pjcmt_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TestePraticoJavaPjcmtApiApplication {
@@ -10,4 +13,14 @@ public class TestePraticoJavaPjcmtApiApplication {
         SpringApplication.run(TestePraticoJavaPjcmtApiApplication.class, args);
     }
 
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                WebMvcConfigurer.super.addCorsMappings(registry);
+            }
+        };
+    }
 }
